b8d6a4a796be0ef8e10ab92535d11f5f12d88e1d
pavel <paveltextfree@gmail.com>
2018-04-23 16:32:43 +0300

    auth and redirect

--- src/containers/MainPageContainer.js
+++ src/containers/MainPageContainer.js
@@ -1,6 +1,5 @@
 import MainPage from '../components/MainPage'
 import { connect } from 'react-redux'
-import { getDataBase } from '../selectors/database'
 import { getUserTokenId } from '../selectors/auth'
 
 const mapStateToProps = state => ({
